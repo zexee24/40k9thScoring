112e677e71f8fa3f826e7e78841f47a89a6c4910
zexee <jerehasu04@gmail.com>
2020-11-13 13:18:50 +0200

    Added the ability to set cp and started to add ability to choose mission.

--- app/src/main/java/com/erej/a40k9thScoring/createBattleFragments/FragmentDetermineMission.kt
+++ app/src/main/java/com/erej/a40k9thScoring/createBattleFragments/FragmentDetermineMission.kt
@@ -2,15 +2,25 @@ package com.erej.a40k9thScoring.createBattleFragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
 import com.erej.a40k9thScoring.R
-import com.erej.a40k9thScoring.battleViewModel
 import com.erej.a40k9thScoring.classes.Battle
+import kotlinx.android.synthetic.main.fragment_create_select_mission.*
 
 class FragmentDetermineMission(val battleObject:Battle): Fragment(R.layout.fragment_create_select_mission) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        val image = when (battleObject.battleType){
+            "Combat patrol" -> R.drawable.combat_patrol
+            "Incursion" -> R.drawable.incursion
+            "Strike force" -> R.drawable.strike_force
+            "Onslaught" -> R.drawable.onslaught
+            else -> throw error("Error in mission types")
+        }
+        imageView.setImageResource(image)
+        val missionPacks = listOf<String>("Core")
+        spinnerSelectMissionPack.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, missionPacks)
     }
 }
--- app/src/main/java/com/erej/a40k9thScoring/createBattleFragments/FragmentMusterArmies.kt
+++ app/src/main/java/com/erej/a40k9thScoring/createBattleFragments/FragmentMusterArmies.kt
@@ -2,15 +2,26 @@ package com.erej.a40k9thScoring.createBattleFragments
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.erej.a40k9thScoring.R
 import com.erej.a40k9thScoring.battleViewModel
 import com.erej.a40k9thScoring.classes.Battle
+import kotlinx.android.synthetic.main.fragment_create_muster_armies.*
 
 class FragmentMusterArmies(val battleObject:Battle): Fragment(R.layout.fragment_create_muster_armies) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+
+    }
+
+    override fun onDestroyView() {
+        battleObject.p1Cp = editTextNumber.text.toString().toInt()
+        battleObject.p2Cp = editTextNumber2.text.toString().toInt()
+        battleViewModel.update(battleObject)
+        super.onDestroyView()
     }
 }
