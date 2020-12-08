bbac5959e09d7507d24f719e2b0ea29c7c52f6da
zexee <jerehasu04@gmail.com>
2020-12-08 09:29:11 +0200

    FragmentSelectSecondaryObjectives.kt nearly done

--- app/src/main/java/com/erej/a40k9thScoring/createBattleFragments/FragmentReadMissionBriefing.kt
+++ app/src/main/java/com/erej/a40k9thScoring/createBattleFragments/FragmentReadMissionBriefing.kt
@@ -6,11 +6,18 @@ import androidx.fragment.app.Fragment
 import com.erej.a40k9thScoring.R
 import com.erej.a40k9thScoring.battleViewModel
 import com.erej.a40k9thScoring.classes.Battle
+import kotlinx.android.synthetic.main.fragment_create_read_briefing.*
 
 class FragmentReadMissionBriefing(val battleObject:Battle): Fragment(R.layout.fragment_create_read_briefing) {
+    val mission = battleObject.primaryMission
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        textViewSpecialRules.text = mission.missionRules
+        textViewPrimaryName.text = mission.primaryObjective.name
+        textViewPrimary.text = mission.primaryObjective.hint
 
+        textViewSecondaryName.text = mission.secondaryObjective.name
+        textViewSecondary.text = mission.secondaryObjective.hint
     }
 }
