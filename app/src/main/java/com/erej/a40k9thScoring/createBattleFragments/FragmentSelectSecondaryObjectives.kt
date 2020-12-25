78c24dda6737e495248d246dab0e3aab68b26637
zexee24 <31950088+zexee24@users.noreply.github.com>
2020-12-25 23:01:04 +0200

    Made a custom adapter for selecting secondaries, still needs some work.

--- app/src/main/java/com/erej/a40k9thScoring/createBattleFragments/FragmentSelectSecondaryObjectives.kt
+++ app/src/main/java/com/erej/a40k9thScoring/createBattleFragments/FragmentSelectSecondaryObjectives.kt
@@ -3,10 +3,12 @@ package com.erej.a40k9thScoring.createBattleFragments
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
-import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.Fragment
 import com.erej.a40k9thScoring.R
 import com.erej.a40k9thScoring.classes.Battle
+import com.erej.a40k9thScoring.classes.Objective
+import com.erej.a40k9thScoring.classes.SecondaryAdapter
 import com.erej.a40k9thScoring.classes.SecondaryList
 import kotlinx.android.synthetic.main.fragment_create_select_secondaries.*
 
@@ -15,130 +17,81 @@ class FragmentSelectSecondaryObjectives(val battleObject:Battle): Fragment(R.lay
     private val allSecondaries = SecondaryList().getSecondaries
 
 
+    private fun setAdapter(spinner: Spinner){
+        spinner.adapter = SecondaryAdapter(activity?.applicationContext!!, allSecondaries)
+    }
 
 
 
-    private fun updateSpinners(){
-        val p1S1List = allSecondaries.toMutableList()
-        val p1S2List = allSecondaries.toMutableList()
-        val p1S3List = allSecondaries.toMutableList()
-
-        val p2S1List = allSecondaries.toMutableList()
-        val p2S2List = allSecondaries.toMutableList()
-        val p2S3List = allSecondaries.toMutableList()
-
-        p1S1List.removeAll {
-            it.category == battleObject.p1Secondary2.category
-            it.category == battleObject.p1Secondary3.category
-        }
-        p1S2List.removeAll {
-            it.category == battleObject.p1Secondary1.category
-            it.category == battleObject.p1Secondary3.category
-        }
-        p1S3List.removeAll {
-            it.category == battleObject.p1Secondary2.category
-            it.category == battleObject.p1Secondary1.category
-        }
-
-
-        p2S1List.removeAll {
-            it.category == battleObject.p2Secondary2.category
-            it.category == battleObject.p2Secondary3.category
-        }
-        p2S2List.removeAll {
-            it.category == battleObject.p2Secondary1.category
-            it.category == battleObject.p2Secondary3.category
-        }
-        p2S3List.removeAll {
-            it.category == battleObject.p2Secondary2.category
-            it.category == battleObject.p2Secondary1.category
-        }
-
-        val p1S1NameList = p1S1List.map { it.name }
-        val p1S2NameList = p1S2List.map { it.name }
-        val p1S3NameList = p1S3List.map { it.name }
-
-        val p2S1NameList = p2S1List.map { it.name }
-        val p2S2NameList = p2S2List.map { it.name }
-        val p2S3NameList = p2S3List.map { it.name }
 
 
 
-        spinnerP1Secondary1.adapter = ArrayAdapter<String>(activity?.applicationContext!!, R.layout.support_simple_spinner_dropdown_item, p1S1NameList)
-        spinnerP1Secondary2.adapter = ArrayAdapter<String>(activity?.applicationContext!!, R.layout.support_simple_spinner_dropdown_item, p1S2NameList)
-        spinnerP1Secondary3.adapter = ArrayAdapter<String>(activity?.applicationContext!!, R.layout.support_simple_spinner_dropdown_item, p1S3NameList)
 
-        spinnerP2Secondary1.adapter = ArrayAdapter<String>(activity?.applicationContext!!, R.layout.support_simple_spinner_dropdown_item, p2S1NameList)
-        spinnerP2Secondary2.adapter = ArrayAdapter<String>(activity?.applicationContext!!, R.layout.support_simple_spinner_dropdown_item, p2S2NameList)
-        spinnerP2Secondary3.adapter = ArrayAdapter<String>(activity?.applicationContext!!, R.layout.support_simple_spinner_dropdown_item, p2S3NameList)
+    private fun createSpinners(){
+        setAdapter(spinnerP1Secondary1)
+        setAdapter(spinnerP1Secondary2)
+        setAdapter(spinnerP1Secondary3)
+
+        setAdapter(spinnerP2Secondary1)
+        setAdapter(spinnerP2Secondary2)
+        setAdapter(spinnerP2Secondary3)
+
+
 
         spinnerP1Secondary1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                battleObject.p1Secondary1 = p1S1List[p2]
-                updateSpinners()
-            }
 
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("Not yet implemented")
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                battleObject.p1Secondary1 = spinnerP1Secondary1.adapter.getItem(position) as Objective
+                val thisSecondary = battleObject.p1Secondary1
+
+                (spinnerP1Secondary2.adapter as SecondaryAdapter).updateList(thisSecondary.category, battleObject.p1Secondary3.category)
+                (spinnerP1Secondary3.adapter as SecondaryAdapter).updateList(thisSecondary.category, battleObject.p1Secondary2.category)
             }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
         }
         spinnerP1Secondary2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                battleObject.p1Secondary2 = p1S2List[p2]
-                updateSpinners()
-            }
 
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("Not yet implemented")
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                battleObject.p1Secondary2 = spinnerP1Secondary1.adapter.getItem(position) as Objective
+                val thisSecondary = battleObject.p1Secondary2
+                (spinnerP1Secondary1.adapter as SecondaryAdapter).updateList(thisSecondary.category, battleObject.p1Secondary3.category)
+                (spinnerP1Secondary3.adapter as SecondaryAdapter).updateList(thisSecondary.category, battleObject.p1Secondary1.category)
             }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
         }
         spinnerP1Secondary3.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                battleObject.p1Secondary3 = p1S3List[p2]
-                updateSpinners()
-            }
 
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("Not yet implemented")
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                battleObject.p1Secondary3 = spinnerP1Secondary1.adapter.getItem(position) as Objective
+                val thisSecondary = battleObject.p1Secondary3
+                (spinnerP1Secondary2.adapter as SecondaryAdapter).updateList(thisSecondary.category, battleObject.p1Secondary1.category)
+                (spinnerP1Secondary1.adapter as SecondaryAdapter).updateList(thisSecondary.category, battleObject.p1Secondary2.category)
             }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
         }
 
-        spinnerP2Secondary1.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                battleObject.p2Secondary1 = p2S1List[p2]
-                updateSpinners()
-            }
 
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
-        }
-        spinnerP2Secondary2.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                battleObject.p2Secondary2 = p2S2List[p2]
-                updateSpinners()
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
-        }
-        spinnerP2Secondary3.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
-            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                battleObject.p2Secondary3 = p2S3List[p2]
-                updateSpinners()
-            }
-
-            override fun onNothingSelected(p0: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
-        }
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        updateSpinners()
+        createSpinners()
 
 
     }
